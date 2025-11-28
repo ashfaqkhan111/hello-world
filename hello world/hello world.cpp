3b19a9b7aa9ea134b666b4448f4248f9b412ce0f
Ashfaq Khan <ahfaqkhan97@gmail.com>
2025-11-28 23:35:14 +0700

    Add project files.

---
+++ hello world/hello world.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main() {
+	
+	double length;
+	double width;
+    
+	cout << "enter length : ";
+	cin >>length;
+	cout << "Enter width : ";
+	cin >> width;
+
+	double area = length * width;
+
+	cout << "the total area is : " <<area;
+	
+
+}
